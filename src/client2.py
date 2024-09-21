050054a9264d80253a3b516fa6b8fc3bb6eb227d
seungjun <kwakseungjun6@gmail.com>
2024-09-21 17:48:42 +0900

    실시간 스트리밍 가능하도록 수정

--- src/client2.py
+++ src/client2.py
@@ -1,9 +1,11 @@
 import requests
 import time
 import cv2
+import os
 
-url = 'http://211.210.140.20:8000/photo'
-filename = "../images/test2.jpg"
+url = 'http://192.168.55.206:8000/photo'
+filename = "./images/temp.jpg"
+osdata = data = os.popen("cat /proc/cpuinfo | grep Serial | awk '{print $3}'").read().strip()
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -11,12 +13,15 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while cv2.waitKey(33) < 0:
     ret, frame = cap.read()
-    # with open(filename, "rb") as f:
-    #     contents = f.read()
+    cv2.imwrite(filename, frame)
 
-    files = {"file": (filename, frame, "image/jpg")}
+    currenttime = time.strftime('%Y%m%d%H%M%S')
+    with open(filename, "rb") as f:
+        contents = f.read()
+    files = {"file": (currenttime+"_"+osdata+".jpg", contents, "image/jpg")}
 
     response = requests.post(url, files=files)
     print(response.json())
+    time.sleep(1)
 
 cap.release()
--- src/main.py
+++ src/main.py
@@ -1,18 +1,39 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, HTTPException, Response
+from fastapi.responses import JSONResponse, StreamingResponse
 import os
-import uuid
 import yolo
 import cv2
 
 app = FastAPI()
 
-def createDirectory(directory):
+hardware_info = [
+    "00000000a1d92ca0",
+    "100000002f35618c"
+]
+
+
+def createdirectory(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
         print("Error: Failed to create the directory.")
+
+
+def streaming(directory):
+    while True:
+        img = cv2.imread(directory)
+        if img is None:
+            continue  # 이미지 파일이 없을 경우 다시 시도
+
+        # 이미지를 JPEG 형식으로 인코딩
+        ret, buffer = cv2.imencode('.jpg', img)
+        if not ret:
+            continue  # 인코딩 실패 시 다시 시도
+
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.post("/photo")
 async def upload_file(file: UploadFile):
@@ -22,7 +43,7 @@ async def upload_file(file: UploadFile):
     # 경로
     directory = '../photo/' + data[1]
 
-    createDirectory(directory)
+    createdirectory(directory)
 
     with open(os.path.join(directory, filename), "wb") as fp:
         fp.write(content)
@@ -30,6 +51,12 @@ async def upload_file(file: UploadFile):
     frame = yolo.check_bug(directory)
     cv2.imwrite(directory + '/' + data[0] + '.jpg', frame)
     return JSONResponse({"filename" : file.filename})
+
+@app.get("/video/{image_num}")
+def send_image(image_num: int):
+    rpi = hardware_info[image_num - 1]
+    directory = f"../photo/{rpi}/temp.jpg"
+    return StreamingResponse(streaming(directory), media_type='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     import uvicorn
