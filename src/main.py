77cb6cabd8c7cb6e9f49ffda4b70b28a83f4ceff
seungjun <kwakseungjun6@gmail.com>
2024-09-22 16:07:07 +0900

    이미지 임시저장 방식 변경

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import yolo
 import cv2
+import shutil
 
 app = FastAPI()
 
@@ -37,8 +38,9 @@ def streaming(directory):
 
         # 이미지를 JPEG 형식으로 인코딩
         ret, buffer = cv2.imencode('.jpg', img)
-        if not ret:
+        if not ret or buffer is None or len(buffer) == 0:
             continue  # 인코딩 실패 시 다시 시도
+
 
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
@@ -52,15 +54,22 @@ async def upload_file(file: UploadFile):
     data = file.filename.split('.')
     # 경로
     directory = '../photo/' + data[0]
-
     create_directory(directory)
+
+    # 기존 temp2.jpg 파일이 존재하면 temp.jpg로 변경
+    old_temp2 = os.path.join(directory, "temp2.jpg")
+    old_temp = os.path.join(directory, "temp.jpg")
+
+    if os.path.exists(old_temp2):
+        shutil.move(old_temp2, old_temp)
 
     with open(os.path.join(directory, filename), "wb") as fp:
         fp.write(content)
     # 경로
     frame = yolo.check_bug(directory)
-    # cv2.imwrite(directory + '/' + data[0] + '.jpg', frame)
-    cv2.imwrite(directory + '/temp.jpg', frame)
+    # temp2.jpg에 분석된 이미지를 저장
+    cv2.imwrite(os.path.join(directory, "temp2.jpg"), frame)
+
     return JSONResponse({"filename": file.filename})
 
 
